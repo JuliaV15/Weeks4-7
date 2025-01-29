ce30b13529e2e2a7d61f460fde16d8503f55e556
versaeve <versaeve@OA-S141-04>
2025-01-29 10:43:31 -0500

    attempted to enable/disable

---
+++ Assets/scripts/enable and disable.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enableanddisable : MonoBehaviour
+{
+    public GameObject go;
+    public SpriteRenderer sr;
+    public EnableDisable script;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0));
+        {
+            // sr.enabled = false;
+            //script.enabled = false;
+            go.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //sr.enabled = true;
+            //script.enabled = true;
+            go.SetActive(true);
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
+            
+        }
+    }
+}
