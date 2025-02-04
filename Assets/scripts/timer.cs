2c877d06cd0199f4ebecc16f1da54c5d387a4ea8
versaeve <versaeve@OA-S141-25>
2025-02-04 16:46:53 -0500

    finished codign gym challenge 1, made sprite move using wasd

--- Assets/scripts/timer.cs
+++ Assets/scripts/timer.cs
@@ -16,7 +16,7 @@ public class timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        t += Time deltaTime;
-        slider.value = 
+       // t += Time deltaTime;
+       // slider.value = 
     }
 }
--- Assets/scripts/week 5 gym.cs
+++ Assets/scripts/week 5 gym.cs
@@ -9,6 +9,7 @@ public class week5gym : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
     Vector3 vectorUp;
+    int movementspeed = 10;
    // int MoveSpeed = 0.5;
 
     // Start is called before the first frame update
@@ -22,8 +23,27 @@ public class week5gym : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-           //  transform.Translate(Vector3, MoveSpeed, 0, 0)
+            //  transform.Translate(Vector3, MoveSpeed, 0, 0)
+            transform.Translate(Vector3.up * movementspeed * Time.deltaTime);
             
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * movementspeed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * movementspeed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * movementspeed * Time.deltaTime);
+
         }
     }
 }
