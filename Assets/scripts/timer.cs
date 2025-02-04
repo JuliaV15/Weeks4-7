61eda06d2ce6fd8084b51102cabd2b3bec3be8f0
versaeve <versaeve@OA-S141-25>
2025-02-04 16:18:08 -0500

    added week 5 assets + staretd the slider

---
+++ Assets/scripts/timer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    Slider slider;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time deltaTime;
+        slider.value = 
+    }
+}
