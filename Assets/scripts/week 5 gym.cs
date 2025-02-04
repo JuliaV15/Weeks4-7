571cb116a695e14a2526cddc7677c7ccb11c8d44
versaeve <versaeve@OA-S141-25>
2025-02-04 16:40:29 -0500

    started coding challenge 1

---
+++ Assets/scripts/week 5 gym.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class week5gym : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Vector3 vectorUp;
+   // int MoveSpeed = 0.5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+           //  transform.Translate(Vector3, MoveSpeed, 0, 0)
+            
+        }
+    }
+}
